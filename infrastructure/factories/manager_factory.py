4a3ec07f7962ceeb0e231d1075eabd7afb7b9a68
noakatz465 <noakatz465@gmail.com>
2026-01-08 11:25:10 +0200

    Change factories to create correct manager

--- infrastructure/factories/manager_factory.py
+++ infrastructure/factories/manager_factory.py
@@ -1,7 +1,9 @@
 from infrastructure.factories.infrastructure_factory import InfrastructureFactory
 from globals.consts.const_strings import ConstStrings
 from infrastructure.interfaces.iexample_manager import IExampleManager
+from infrastructure.interfaces.ivideo_manager import IVideoManager
 from model.managers.example_manager import ExampleManager
+from model.managers.video_manager import VideoManager
 
 
 class ManagerFactory:
@@ -12,5 +14,11 @@ class ManagerFactory:
         return ExampleManager(config_manager)
 
     @staticmethod
+    def create_video_manager() -> IVideoManager:
+        config_manager = InfrastructureFactory.create_config_manager(
+            ConstStrings.GLOBAL_CONFIG_PATH)
+        return VideoManager(config_manager)
+
+    @staticmethod
     def create_all() -> None:
-        ManagerFactory.create_example_manager().start()
+        ManagerFactory.create_video_manager().start()
