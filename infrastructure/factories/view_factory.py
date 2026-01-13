50e2a1c76c33c80e6a904aa9b2e0336a5cb22203
meiriluz <meiriluz12@gmail.com>
2026-01-13 22:18:47 +0200

    Update view factory to build the region widgets

--- infrastructure/factories/view_factory.py
+++ infrastructure/factories/view_factory.py
@@ -1,12 +1,27 @@
-from view.main_window import MainWindow
+from globals.enums.enums import VideoLayoutRegionKey
 from view.pages.video_page import VideoPage
+from view.regions.video_region_widget import VideoRegionWidget
+from view.regions.info_region_widget import InfoRegionWidget
+from view.main_window import MainWindow
 from view_model.main_window_view_model import MainWindowViewModel
+
 
 class ViewFactory:
     @staticmethod
-    def create_main_window():
+    def create_main_window() -> MainWindow:
         vm = MainWindowViewModel()
-        video_page = VideoPage(vm)
-
-        pages = [video_page]  # later you can add more pages
+        video_page = ViewFactory.create_video_page(vm)
+        pages = [video_page]
         return MainWindow(vm, pages)
+
+    @staticmethod
+    def create_video_page(vm: MainWindowViewModel) -> VideoPage:
+        video_region = VideoRegionWidget(vm)
+        info_region = InfoRegionWidget(vm)
+
+        regions = {
+            VideoLayoutRegionKey.VIDEO_REGION: video_region,
+            VideoLayoutRegionKey.INFO_REGION: info_region,
+        }
+
+        return VideoPage(vm, regions)
